6456290f273d20876769d1daea4ea3dbd426ed00
subarnadas1510-netizen <subarna.das1510@gmail.com>
2026-07-30 21:23:06 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = "localhost";
+$db_user = "ami";
+$db_pass = "1234";
+$db_name = "cms";
+
+$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+if (mysqli_connect_error())
+ {
+	echo mysqli_connect_error() ;
+	exit;
+}
+?>
